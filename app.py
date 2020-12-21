bee8a7f685f332b57e92ab9f4cd69ada0198c289
watcharap0n <69833839+watcharap0n@users.noreply.github.com>
2020-12-21 16:15:08 +0700

    g

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def contact():
    return render_template('formContact.html')
 
 
+@app.route('/post_data')
+def post_data():
+    post_data = request.get_json()
+    print(post_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5510)
