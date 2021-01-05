da9f10940736f2ffa209dd97e790d6b27514bf7d
Wichayut Ketsith <wichayut.ks@gmail.com>
2021-01-05 14:45:56 +0700

    Layout Status

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask.helpers import make_response
 
 
 app = Flask(__name__)
@@ -15,14 +16,33 @@ def contact():
    return render_template('formContact.html')
 
 
+@app.route('/status')
+def status():
+   return render_template('statusView.html')
+
+
 @app.route('/post_data')
 def post_data():
     post_data = request.get_json()
     print(post_data)
 
+
 @app.route('/liff_contact')
 def liff_contact():
    return render_template('/line_liff/lineLiffContact.html')
+
+
+@app.route('/test_login')
+def test_login():
+   return render_template('/line_liff/loginTest.html')
+
+
+@app.route('/sdata', methods=['GET', 'POST']) 
+def sdata():
+    if request.method == 'POST':
+        sdata = request.get_json()
+        print(sdata)
+        return make_response(sdata)
 
 
 if __name__ == '__main__':
